6af70c7a437bcc181cfd834428a33ccb2b535268
Ben Du <bendu@me.com>
2015-02-16 14:24:30 -0800

    Including a bunch of VS stuff

---
+++ Stuff/Stuff/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Console.Write("Hit enter to exit ->");
+            Console.ReadLine();
+        }
+    }
+}
